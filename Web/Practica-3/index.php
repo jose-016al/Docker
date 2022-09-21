eeabc0bab11b3193e2e26d13553f68878d3917cc
jose-016al <jose_016@outlook.com>
2022-09-21 11:27:52 +0200

    Change web

--- Web/Practica-3/index.php
+++ Web/Practica-3/index.php
@@ -2,34 +2,81 @@
 <html lang="en">
     <head>
         <title>Practica 3</title>
+        <link rel="stylesheet" href="style.css">
         <?php
         $_GET["seccion"];
+        $menu = [
+            "./?seccion=inicio" => "Inicio",
+            "./?seccion=galeria" => "Galeria",
+            "./?seccion=contacto" => "Contacto" 
+        ];
+        $galeria = [
+            "./img/foto1.jpg",
+            "./img/foto2.jpg",
+            "./img/foto3.jpg",
+            "./img/foto4.jpg",
+            "./img/foto5.jpg",
+            "./img/foto6.jpg",
+            "./img/foto7.jpg",
+            "./img/foto8.jpg",
+            "./img/foto9.jpg"
+        ];
         ?>
     </head>
     <body>
-        <h1>Practica 3</h1>
-        <nav>
-            <ol><a href="index.php">inicio</a></ol>
-            <ol><a href="contacto.html">contacto</a></ol>
-            <ol><a href="galeria.php">galeria</a></ol>
-        </nav>
-        
-        <?php
-            if ($_GET["seccion"] == "inicio") {
-                // <p>Esta es mi pagina web</p>
-                echo '<img src=" ' . "img/foto1.jpg" . ' " width ="100%">';
-            } else if ($_GET["seccion"] == "galeria") {
-                $galeria = [
-                    "img/foto1.jpg",
-                    "img/foto2.jpg",
-                    "img/foto3.jpg"
-                ];
-                foreach ($galeria as $imagen) {
-                    echo '<img src=" ' . $imagen . ' " width ="100%">';
+        <header>
+            <h1>Practica 3</h1>
+            <nav>
+                <?php
+                echo '<ul>';
+                    foreach ($menu as $valor => $clave) {
+                        echo '<li class=enlaces><a href=' . $valor . '>' . $clave .  '</a></li>';
+                    }
+                echo '</ul>';
+                ?>
+            </nav>
+        </header>
+        <section>
+            <?php
+            if (isset($_GET["seccion"])) {
+                switch ($_GET["seccion"]) {
+                    case "inicio":
+                        echo "<p>Esta es mi pagina web</p>";
+                        echo '<img src=" ' . "./img/foto1.jpg" . ' " width ="50%">';
+                        break;
+                    case "galeria":
+                        echo '<table>';
+                        foreach ($galeria as $indice => $imagen) {
+                            if ($indice % 3 == 0) {
+                                echo '<tr>';
+                            }
+                            echo '<td>
+                            <a href='. $imagen .'>
+                            <img src=" ' . $imagen . ' " width ="100%" height="250px"></a>
+                            </td>';
+                        }
+                        echo '</tr></table>';
+                        break;
+                    case "contacto":
+                        echo '
+                        <form action="">
+                            <label>Nombre</label>
+                            <input type="text">
+                            <label>Apellidos</label>
+                            <input type="text">
+                            <label>Telefono</label>
+                            <input type="text">
+                            <label>Email</label>
+                            <input type="email">
+                        </form>
+                        ';
+                        break;
                 }
-            } else if ($_GET["seccion"] == "contacto") {
-                
+            } else {
+                echo "<p>Esta es mi pagina web</p>";
+                echo '<img src=" ' . "./img/foto1.jpg" . ' " width ="50%">';
             }
-        ?>
+            ?>
+        </section>
     </body>
 </html>
